6a84838a67aeaaf54793fba7a2b7682699d58a8d
sheridan gomes <sheridangomes@ymail.com>
2021-07-26 19:49:12 +1000

    Initial Commit

---
+++ Brocolli&Co/Brocolli_CoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brocolli_CoApp.swift
+//  Brocolli&Co
+//
+//  Created by user201819 on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Brocolli_CoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brocolli&Co/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Brocolli&Co
+//
+//  Created by user201819 on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
