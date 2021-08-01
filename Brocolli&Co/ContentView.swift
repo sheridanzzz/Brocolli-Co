67eef1ad5303359e57583761a08bd67fb20eadaf
sheridanzzz <sheridangomes@ymail.com>
2021-08-02 01:58:29 +1000

    new update

--- Brocolli&Co/ContentView.swift
+++ Brocolli&Co/ContentView.swift
@@ -13,29 +13,45 @@ import SystemConfiguration
 
 struct ContentView: View {
     @State private var isPresented = false
-    
+    @State var isNavBarHidden = false
+    //user default get the value stored
+    @State var isLoggedin: Bool =  UserDefaults.standard.bool(forKey: "myLogin")
     //email check backend validation: usedemail@blinq.app
     //first view
     var body: some View {
-        NavigationView {
-            VStack() {
-                Image("Brocolli_and_Co")
-                    .resizable()
-                    .scaledToFit()
-                Text("Welcome to Brocolli & Co.").padding()
-                Button(action: {
-                    self.isPresented.toggle()
-                }, label: {
-                    Text("Request Invite")
-                        .bold()
-                        .foregroundColor(Color.white)
-                        .frame(width: 200, height: 50, alignment: .center)
-                        .background(Capsule().fill(Color.green))
-                })
-                //to modally load next screen
-                .fullScreenCover(isPresented: $isPresented, content: InviteView.init)
+        //load different view if user asked for invite
+        if(isLoggedin)
+        {
+            CancelInviteView()
+        } else {
+            NavigationView {
+                VStack() {
+                    Image("Brocolli_and_Co")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Text("Welcome to Brocolli & Co.").padding()
+                    Button(action: {
+                        self.isPresented.toggle()
+                    }, label: {
+                        Text("Request Invite")
+                            .bold()
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 50, alignment: .center)
+                            .background(Capsule().fill(Color.green))
+                    })
+                    //to modally load next screen
+                    .fullScreenCover(isPresented: $isPresented, content: InviteView.init)
+                }
+                .navigationBarTitle("Brocolli & Co.")
+                .navigationBarBackButtonHidden(self.isNavBarHidden)
+                .onAppear {
+                    self.isNavBarHidden = true
+                }.onDisappear {
+                    self.isNavBarHidden = false
+                }
+                
             }
-            .navigationTitle("Brocolli & Co.")
         }
     }
 }
@@ -54,73 +70,73 @@ struct InviteView: View {
     @State var isclosed: Bool = true
     
     var body: some View{
-        NavigationView {
-            ZStack {
+        ZStack {
+            VStack {
                 VStack {
-                    VStack {
-                        //Brocolli and co image
-                        Image("Brocolli_and_Co")
-                            .resizable()
-                            .scaledToFit()
-                        //text fields for name and email
-                        EntryField(sfSymbolName: "person", placeHolder: "Full Name", prompt: inviteVm.fullnamePrompt, field: $inviteVm.fullName)
-                        EntryField(sfSymbolName: "envelope", placeHolder: "Email Address", prompt: inviteVm.emailPrompt, field: $inviteVm.email)
-                        EntryField(sfSymbolName: "envelope", placeHolder: "Confirm Email Address", prompt: inviteVm.confirmEmPrompt, field: $inviteVm.confirmEm)
-                        ZStack{
-                            //loading screen in zStack
-                            LoaderView(tintColor: .green, scaleSize: 1.0).padding(.bottom,50).hidden(isHideLoader)
-                            HStack{
-                                Button(action: {
-                                    self.isHideLoader = !self.isHideLoader
-                                    let secondsToDelay = 5.0
-                                    //call function to call API
-                                    self.errMod.checkDetails(name: inviteVm.fullName, email: inviteVm.email)
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + secondsToDelay) {
-                                        //hide the loader
-                                        self.isHideLoader.toggle()
-                                    }
-                                }) {
-                                    Text("Send")
-                                        .foregroundColor(.white)
-                                        .padding(.vertical, 5)
-                                        .padding(.horizontal)
-                                        .background(Capsule().fill(Color.green))
+                    //Brocolli and co image
+                    Image("Brocolli_and_Co")
+                        .resizable()
+                        .scaledToFit()
+                    //text fields for name and email
+                    EntryField(sfSymbolName: "person", placeHolder: "Full Name", prompt: inviteVm.fullnamePrompt, field: $inviteVm.fullName)
+                    EntryField(sfSymbolName: "envelope", placeHolder: "Email Address", prompt: inviteVm.emailPrompt, field: $inviteVm.email)
+                    EntryField(sfSymbolName: "envelope", placeHolder: "Confirm Email Address", prompt: inviteVm.confirmEmPrompt, field: $inviteVm.confirmEm)
+                    ZStack{
+                        //loading screen in zStack
+                        LoaderView(tintColor: .green, scaleSize: 1.0).padding(.bottom,50).hidden(isHideLoader)
+                        HStack{
+                            Button(action: {
+                                self.isHideLoader = !self.isHideLoader
+                                let secondsToDelay = 5.0
+                                //call function to call API
+                                self.errMod.checkDetails(name: inviteVm.fullName, email: inviteVm.email)
+                                DispatchQueue.main.asyncAfter(deadline: .now() + secondsToDelay) {
+                                    //hide the loader
+                                    self.isHideLoader.toggle()
                                 }
-                                //alert box with showing alert is triggered
-                                .alert(isPresented: $errMod.showingAlert, content: {
-                                    Alert(title: Text("Error"), message: Text(errMod.errMess), dismissButton: .default(Text("OK")))
-                                })
-                                //button disabled until validation complete
-                                .opacity(inviteVm.isInviteComplete ? 1 : 0.6)
-                                .disabled(!inviteVm.isInviteComplete)
-                                Button(action: {
-                                    //dismiss view on cancel
-                                    presentationMode.wrappedValue.dismiss()
-                                }) {
-                                    Text("cancel")
-                                        .foregroundColor(.white)
-                                        .padding(.vertical, 5)
-                                        .padding(.horizontal)
-                                        .background(Capsule().fill(Color.green))
-                                }
+                            }) {
+                                Text("Send")
+                                    .foregroundColor(.white)
+                                    .padding(.vertical, 5)
+                                    .padding(.horizontal)
+                                    .background(Capsule().fill(Color.green))
+                            }
+                            //alert box with showing alert is triggered
+                            .alert(isPresented: $errMod.showingAlert, content: {
+                                Alert(title: Text("Error"), message: Text(errMod.errMess), dismissButton: .default(Text("OK")))
+                            })
+                            //button disabled until validation complete
+                            .opacity(inviteVm.isInviteComplete ? 1 : 0.6)
+                            .disabled(!inviteVm.isInviteComplete)
+                            Button(action: {
+                                //dismiss view on cancel
+                                presentationMode.wrappedValue.dismiss()
+                            }) {
+                                Text("cancel")
+                                    .foregroundColor(.white)
+                                    .padding(.vertical, 5)
+                                    .padding(.horizontal)
+                                    .background(Capsule().fill(Color.green))
                             }
                         }
                     }
-                    .padding()
-                    .background(Color(UIColor.systemBackground))
-                    .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                    .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-                    
-                    Spacer()
                 }
-                .navigationTitle("Invite Details")
                 .padding()
-                //move to next view on trigger
-                .navigate(to: CongratulationsView(), when: $errMod.success)
-            } // End of ZStack
+                .background(Color(UIColor.systemBackground))
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                
+                Spacer()
+            }
+            .navigationBarTitle("Invite Details")
+            .padding()
             
-            
-        }
+            //move to next view on trigger
+            .navigate(to: CongratulationsView(), when: $errMod.success)
+        } // End of ZStack
+        
+        
+        
         
     }
 }
@@ -136,13 +152,13 @@ extension View {
         NavigationView {
             ZStack {
                 self
-                    .navigationBarTitle("")
-                    .navigationBarHidden(true)
+                    .navigationBarBackButtonHidden(true)
                 
                 NavigationLink(
                     destination: view
-                        .navigationBarTitle("")
-                        .navigationBarHidden(true),
+                        .navigationBarHidden(true)
+                        .navigationBarBackButtonHidden(true)
+                    ,
                     isActive: binding
                 ) {
                     EmptyView()
@@ -157,65 +173,69 @@ struct CongratulationsView: View {
     @State var counter7:Int = 0
     @State private var isDismissed = false
     @State var isDone: Bool = false
+    @State private var isLogggedin = true
     
     var body: some View {
-        NavigationView {
-            VStack{
-                ZStack{
-                    //confetti
-                    Text("🥦").font(.system(size: 50)).onTapGesture(){counter7 += 1}
-                    //library for confetti functions
-                    ConfettiCannon(counter: $counter7, num:1, confettis: [.text("🍆"), .text("🥕"), .text("🍠"), .text("🥬")], confettiSize: 30, repetitions: 50, repetitionInterval: 0.1)
-                }
-                Text("Congratulation! Invite Sent!")
+        VStack{
+            ZStack{
+                //confetti
+                Text("🥦").font(.system(size: 50)).onAppear(){counter7 += 1}
+                //library for confetti functions
+                ConfettiCannon(counter: $counter7, num:1, confettis: [.text("🍆"), .text("🥕"), .text("🍠"), .text("🥬")], confettiSize: 30, repetitions: 50, repetitionInterval: 0.1)
             }
-            //navbar item to close view
-            .toolbar(content: {
-                Button{
-                    isDone.toggle()
-                }label:{
-                    Label("close", systemImage: "xmark")
-                }
-            })
-        }.navigate(to: CancelInviteView(), when: $isDone)
+            .onAppear{
+                UserDefaults.standard.set(isLogggedin, forKey: "myLogin")
+            }
+            Text("Congratulation! Invite Sent!")
+            
+            Button(action: {
+                isDone.toggle()
+                
+            }) {
+                Label("close", systemImage: "xmark")
+            }
+        }
+        .navigate(to: CancelInviteView(), when: $isDone)
     }
 }
 
 struct CancelInviteView: View {
     @State private var isPresented = false
     @State var isCancel: Bool = false
+    @State var isNavBarHidden = false
+    @State private var isLogggedin = false
     
     var body: some View {
-        NavigationView {
-            VStack() {
-                Image("Brocolli_and_Co")
-                    .resizable()
-                    .scaledToFit()
-                Text("Welcome to Brocolli & Co.").padding()
-                Button(action: {
-                    self.isPresented.toggle()
-                }, label: {
-                    Text("Cancel Invite")
-                        .bold()
-                        .foregroundColor(Color.white)
-                        .frame(width: 200, height: 50, alignment: .center)
-                        .background(Capsule().fill(Color.green))
-                })
-                //alert box for delete invite
-                .alert(isPresented: $isPresented) {
-                    Alert(
-                        title: Text("Are you sure you want to delete this?"),
-                        message: Text("Can't Undo"),
-                        primaryButton: .destructive(Text("Delete")) {
-                            isCancel.toggle()
-                        },
-                        secondaryButton: .cancel()
-                    )
-                }
+        VStack() {
+            Image("Brocolli_and_Co")
+                .resizable()
+                .scaledToFit()
+            Text("Welcome to Brocolli & Co.").padding()
+            Button(action: {
+                self.isPresented.toggle()
+            }, label: {
+                Text("Cancel Invite")
+                    .bold()
+                    .foregroundColor(Color.white)
+                    .frame(width: 200, height: 50, alignment: .center)
+                    .background(Capsule().fill(Color.green))
+            })
+            //alert box for delete invite
+            .alert(isPresented: $isPresented) {
+                Alert(
+                    title: Text("Are you sure you want to delete this?"),
+                    message: Text("Can't Undo"),
+                    primaryButton: .destructive(Text("Delete")) {
+                        UserDefaults.standard.set(isLogggedin, forKey: "myLogin")
+                        isCancel.toggle()
+                    },
+                    secondaryButton: .cancel()
+                )
             }
-            .navigationBarHidden(true)
-            .navigate(to: CancelView(), when: $isCancel)
         }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitle("Brocolli & Co.")
+        .navigate(to: CancelView(), when: $isCancel)
     }
 }
 
@@ -223,29 +243,29 @@ struct CancelView: View {
     @State var counter6:Int = 0
     @State private var isDismissed = false
     @State var isDone: Bool = false
+    @State var isNavBarHidden = false
+    
     
     var body: some View {
-        NavigationView {
-            VStack{
-                ZStack{
-                    Text("💩").font(.system(size: 50)).onTapGesture(){counter6 += 1}
-                    ConfettiCannon(counter: $counter6, num:1, confettis: [.text("💩")], confettiSize: 20, repetitions: 100, repetitionInterval: 0.1)
-                    
-                }
-                Text("Invite Cancelled!")
+        VStack{
+            ZStack{
+                Text("💩").font(.system(size: 50)).onAppear(){counter6 += 1}
+                ConfettiCannon(counter: $counter6, num:1, confettis: [.text("💩")], confettiSize: 20, repetitions: 100, repetitionInterval: 0.1)
+                
             }
-            .toolbar(content: {
-                Button{
-                    isDone.toggle()
-                }label:{
-                    Label("close", systemImage: "xmark")
-                }
-            })
-        }.navigate(to: ContentView(), when: $isDone)
+            Text("Invite Cancelled!")
+            
+            Button(action: {
+                isDone.toggle()
+            }) {
+                Label("close", systemImage: "xmark")
+            }
+        }
+        .navigate(to: ContentView(), when: $isDone)
     }
 }
 
-//hide view
+//Hide view
 extension View {
     @ViewBuilder func hidden(_ shouldHide: Bool) -> some View {
         switch shouldHide {
